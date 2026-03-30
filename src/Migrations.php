b6cefe85cf00ccccb5338c40da779914c19ac901
nick <nlo@tro.net>
2026-03-30 17:35:16 +0200

    fix: only generate views if oxconfig is available.

--- src/Migrations.php
+++ src/Migrations.php
@@ -108,6 +108,28 @@ class Migrations
             }
         }
 
+        $dbConfig = include $this->dbFilePath;
+        try {
+            $dsn = sprintf('%s:host=%s;port=%s;dbname=%s', $dbConfig['driver'], $dbConfig['host'], $dbConfig['port'], $dbConfig['dbname']);
+            $pdo = new \PDO($dsn, $dbConfig['user'], $dbConfig['password'], $dbConfig['driverOptions'] ?? []);
+            $stmt = $pdo->query("SELECT COUNT(*) FROM `oxconfig` LIMIT 1");
+            $oxconfigReady = $stmt && $stmt->fetchColumn() > 0;
+        } catch (\Exception $e) {
+            $oxconfigReady = false;
+        }
+
+        if (!$oxconfigReady) {
+            printf("Skipping database views generation: oxconfig table is empty or not accessible.\n");
+            return 0;
+        } else {
+            exec(__DIR__ . '/../../../bin/oe-eshop-db_views_generate', $output, $returnCode);
+
+            if ($returnCode !== 0) {
+                echo('<error>Failed to regenerate database views.</error> \ln');
+                return $returnCode;
+            }
+            printf("Database views generated successfully.\n");
+        }
 
         return 0;
     }
