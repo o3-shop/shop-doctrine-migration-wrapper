65aa076aafa45720ad68e1277bd39072c43163b4
nick <nlo@tro.net>
2026-04-01 15:18:53 +0200

    feat: rewrite DoctrineApplicationBuilder for migrations 3.x DependencyFactory API

--- src/DoctrineApplicationBuilder.php
+++ src/DoctrineApplicationBuilder.php
@@ -3,13 +3,13 @@
 /**
  * This file is part of O3-Shop.
  *
- * O3-Shop is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+ * O3-Shop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
- * O3-Shop is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * O3-Shop is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with O3-Shop.  If not, see <http://www.gnu.org/licenses/>
@@ -23,27 +23,41 @@ declare(strict_types=1);
 
 namespace OxidEsales\DoctrineMigrationWrapper;
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
+use Doctrine\Migrations\Configuration\Migration\YamlFile;
+use Doctrine\Migrations\DependencyFactory;
 use Doctrine\Migrations\Tools\Console\ConsoleRunner;
 use Symfony\Component\Console\Application;
 
 class DoctrineApplicationBuilder
 {
     /**
-     * Return new application for each build.
-     * Application has a reference to command which has internal cache.
-     * Reusing same application object with same command leads to an errors due to an old configuration.
-     * For example first run with a CE migrations
-     * second run with PE migrations
-     * both runs would take path to CE migrations.
+     * Build a fresh Doctrine Migrations console application for the given
+     * migration config and database config files.
+     *
+     * A new application is built for each migration set because each has its
+     * own configuration (namespace, table, directory). Reusing an application
+     * across sets would carry over stale configuration.
+     *
+     * @param string $dbFilePath  Path to a PHP file that returns a DBAL connection params array.
+     * @param string $configPath  Path to a migrations 3.x YAML configuration file.
      *
      * @return Application
      */
-    public function build()
+    public function build(string $dbFilePath, string $configPath): Application
     {
-        $helperSet = new \Symfony\Component\Console\Helper\HelperSet();
-        $doctrineApplication = ConsoleRunner::createApplication($helperSet);
+        $dbConfig = include $dbFilePath;
+        $conn = DriverManager::getConnection($dbConfig);
+
+        $dependencyFactory = DependencyFactory::fromConnection(
+            new YamlFile($configPath),
+            new ExistingConnection($conn)
+        );
+
+        $doctrineApplication = ConsoleRunner::createApplication($dependencyFactory);
         $doctrineApplication->setAutoExit(false);
-        $doctrineApplication->setCatchExceptions(false); // we handle the exception on our own!
+        $doctrineApplication->setCatchExceptions(false);
 
         return $doctrineApplication;
     }
